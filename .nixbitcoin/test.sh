d5a01ed181cbce7533a816435e703d04b04a97bb
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-24 08:13:35 -0300

    Create test.sh

---
+++ .nixbitcoin/test.sh
@@ -0,0 +1,16 @@
+
+
+$ hello
+The program ‘hello’ is currently not installed.
+
+$ nix-shell -p hello
+
+[nix-shell:~]$ hello
+Hello, world!
+
+[nix-shell:~]$ exit
+exit
+
+$ hello
+The program ‘hello’ is currently not installed.
+
