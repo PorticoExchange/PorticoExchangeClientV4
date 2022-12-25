71a9ff5dfca393ed1f2ebcac714a8277ae7bb401
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-12-25 08:39:37 -0300

    Update fabric.c

--- swap/fabric.c
+++ swap/fabric.c
@@ -1,2 +1,11 @@
-import "github.com/FabricLabs/fabric/blob/master/services/lightning.js"
-  
+#ifndef FABRIC_PROTOCOL_LIGHTNING_H
+#define FABRIC_PROTOCOL_LIGHTNING_H
+#include "config.h"
+
+struct ext_key;
+struct lightningd;
+struct db_stmt;
+struct db;
+struct balance;
+struct listInvoice;
+struct generateInvoice;
