d593b019018e7a009e09d11a7b4ea19dbe043f76
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-01-23 07:21:09 -0300

    Update liquid.c

--- swap/liquid.c
+++ swap/liquid.c
@@ -1,3 +1,13 @@
 #ifndef LIQUID_NETWORK_LIGHTNING_H
 #define LIQUID_NETWORK_LIGHTNING_H
 #include "config.h"
+
+
+struct ext_key;
+struct lightningd;
+struct db_stmt;
+struct db;
+struct balance;
+struct listInvoice;
+struct generateInvoice;
+struct receiveInvoice;
